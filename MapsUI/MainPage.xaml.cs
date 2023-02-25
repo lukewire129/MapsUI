87b15bb6c6e83b4bf5ef417e180f4e2df134296c
Luke.Lee <kevin.lgs@aceenter.com>
2023-02-25 22:28:04 +0900

    초기 커밋.

---
+++ MapsUI/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Mapsui.UI.Maui;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+namespace MapsUITest;
+
+public partial class MainPage : ContentPage
+{
+        public MainPage()
+        {
+		InitializeComponent();
+                var mapControl = new MapControl();
+                mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+                Content = mapControl;
+        }
+}
+
